418125286f2382fdbf9f59f0b1867b1bc62d736b
Omar Faruk <60820986+Omar180859@users.noreply.github.com>
2024-10-07 01:06:17 +0600

    Add files via upload

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,51 @@
-num1 = 10
-document.write("The Number is :", num1, "<br>", "Artificial Intelligent Technology")
-console.log(num1)
+// let num1 = 50
+// num2 = 40
+// sum = num1 + num2
+// sub = num1 - num2
+// mul = num1 * num2
+// div = num1 / num2
+// mod = num1 % num2
+// document.write("The Summation  is:", sum, "<br>", "The Substrac is :", sub)
+// document.write("<br>")
+// document.write("Multiplication is :", mul)
+// document.write("<br>")
+// document.write("Division is :", div)
+// document.write("<br>")
+// document.write("Modulas is :", mod)
+// console.log(sum)
+// console.log(sub)
+// #Three types of variable:
+// var
+// let
+// Const
+// var x = "Hello"
+// var x = "world"
+
+// x = "AIT"
+// console.log(x)
+// console.log(x)
+// console.log(x)
+
+/* Different Type of Data Types:
+var x="Hello World";--------->String
+var x=25;---------------->Number
+var x=true -----------------> Boolean
+var x=["HTML","CSS","JS"]-------> Array
+var x={name:"faruk",last:"Hossain"}------->Object
+var x=null----------->null
+var x; --->
+*/
+a = 20;
+b = 15;
+++a; --b;
+c = a++ - b--;
+d = --c + ++a;
+document.write("A =", a, "<br>", "B =", b, "<br>", "C =", c, "<br>", "D =", d);
+
+
+// document.write(2 ** 4); //Exponentiation
+// document.write("<br>");
+
+
+
+
